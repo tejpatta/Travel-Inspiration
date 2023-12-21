f4248d655179087576f804fe0a5a0b6e2d231db2
Dr.Robotnik <jackj.1988@googlemail.com>
2023-12-21 22:38:43 +0000

    fixed carousel nesting and the order in which elements append

--- assets/js/Unsplash.js
+++ assets/js/Unsplash.js
@@ -11,37 +11,22 @@ function displayPhotos(){
         return response.json();
       })
       .then(function(data){
-      // let firstImageDiv = $("<div>").addClass("carousel-item active")
-      // let firstImageCaptionDiv = $("<div>").addClass("carousel-caption d-none d-md-block")
-      // let firstImageCaption = $("<h5>").text(data.results[0].description)
-      // let countryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[0].urls.full)
-      // firstImageDiv.append(firstImageCaptionDiv)
-      // firstImageCaptionDiv.append(countryImage, firstImageCaption)
-      // $(".carousel-inner").append(firstImageDiv)
-
-    //   let firstImageDiv = $('<div class="carousel-item active">\
-    //   <div class="carousel-caption d-none d-md-block">\
-    //     <img class="d-block w-100" src="'+ data.results[0].urls.full +'">\
-    //     <h5>'+ data.results[0].description +'</h5>\
-    //   </div>\
-    // </div>')
-
-    // $(".carousel-inner").append(firstImageDiv)
-
         let firstImageDiv = $("<div>").addClass("carousel-item active")
+        let firstCountryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[0].urls.full)
         let firstImageCaptionDiv = $("<div>").addClass("carousel-caption d-none d-md-block")
         let firstImageCaption = $("<h5>").text(data.results[0].description)
-        let firstcountryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[0].urls.full)
-        firstImageDiv.append(firstcountryImage)
-        $(".carousel-inner").append(firstImageDiv, firstImageCaptionDiv, firstImageCaption)
+        firstImageCaptionDiv.append(firstImageCaption)
+        firstImageDiv.append(firstCountryImage, firstImageCaptionDiv)
+        $(".carousel-inner").append(firstImageDiv)
 
       for (i = 1; i < 3; i++) {
         let ImageDiv = $("<div>").addClass("carousel-item")
+        let CountryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[i].urls.full)
         let ImageCaptionDiv = $("<div>").addClass("carousel-caption d-none d-md-block")
         let ImageCaption = $("<h5>").text(data.results[i].description)
-        let countryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[i].urls.full)
-        ImageDiv.append(countryImage)
-        $(".carousel-inner").append(ImageDiv, ImageCaptionDiv, ImageCaption)
+        ImageCaptionDiv.append(ImageCaption)
+        ImageDiv.append(CountryImage, ImageCaptionDiv)
+        $(".carousel-inner").append(ImageDiv)
       }
 })
 }

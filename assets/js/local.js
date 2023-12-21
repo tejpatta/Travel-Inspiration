3ae9dab6608b6dd464564f73dee18e3f3b6eddb4
Tejpatta <tejurawal@gmail.com>
2023-12-21 19:40:38 +0000

    added results section and established universal variables

---
+++ assets/js/local.js
@@ -0,0 +1,6 @@
+
+let countryInput = $("#country-search").val().trim();
+
+let unsplashKey1 = "nkO4QtA3j8b37iVDzhJIitt4QwUCEv6UfCOB7-O2d1I";
+let unsplashKey2 = "__q3dGBVbr-ZPGFeEpdoAGNhbQ24nZxvv6gIazuJnsA";
+let travelKey1 = "nkO4QtA3j8b37iVDzhJIitt4QwUCEv6UfCOB7-O2d1I";

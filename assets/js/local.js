95f52aa21900af145f4bc27e5fbf7f5df3bae982
James Greaves <jgreavesei@gmail.com>
2023-12-22 19:15:05 +0000

    Added My Favourites button with love hearts

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,6 +1,7 @@
 
 
 
+
 let unsplashKey1 = "nkO4QtA3j8b37iVDzhJIitt4QwUCEv6UfCOB7-O2d1I";
 let unsplashKey2 = "__q3dGBVbr-ZPGFeEpdoAGNhbQ24nZxvv6gIazuJnsA";
 let travelKey1 = "nkO4QtA3j8b37iVDzhJIitt4QwUCEv6UfCOB7-O2d1I";

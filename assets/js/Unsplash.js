db8c2dc76ef5c23aba0f2a5d85dd510d981652ad
Tejpatta <tejurawal@gmail.com>
2023-12-22 19:00:22 +0000

    fit images to standard carousel size

--- assets/js/Unsplash.js
+++ assets/js/Unsplash.js
@@ -3,11 +3,7 @@
 let unsplashKey1 = "nkO4QtA3j8b37iVDzhJIitt4QwUCEv6UfCOB7-O2d1I";
 let unsplashKey2 = "__q3dGBVbr-ZPGFeEpdoAGNhbQ24nZxvv6gIazuJnsA";
 
-<<<<<<< HEAD
 let photoURL = "https://api.unsplash.com/search/photos/?client_id=" + unsplashKey1+ "&query=" + countryInput;
-=======
-let photoURL = " https://api.unsplash.com/search/photos/?client_id=" + unsplashKey1+ "&query=Italy";
->>>>>>> a7c548af6d7f716c521d64db3b5b02a5f704813d
 
 function displayPhotos(){
     fetch(photoURL)
@@ -16,7 +12,7 @@ function displayPhotos(){
       })
       .then(function(data){
         let firstImageDiv = $("<div>").addClass("carousel-item active")
-        let firstCountryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[0].urls.full)
+        let firstCountryImage = $("<img>").addClass("d-block w-100").attr("src", (data.results[0].urls.raw + '&fit=crop&w=550&h=500'))
         let firstImageCaptionDiv = $("<div>").addClass("carousel-caption d-none d-md-block")
         let firstImageCaption = $("<h5>").text(data.results[0].description)
         firstImageCaptionDiv.append(firstImageCaption)
@@ -25,7 +21,7 @@ function displayPhotos(){
 
       for (i = 1; i < 3; i++) {
         let ImageDiv = $("<div>").addClass("carousel-item")
-        let CountryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[i].urls.full)
+        let CountryImage = $("<img>").addClass("d-block w-100").attr("src", data.results[i].urls.raw + '&fit=crop&w=550&h=500')
         let ImageCaptionDiv = $("<div>").addClass("carousel-caption d-none d-md-block")
         let ImageCaption = $("<h5>").text(data.results[i].description)
         ImageCaptionDiv.append(ImageCaption)
@@ -33,10 +29,6 @@ function displayPhotos(){
         $(".carousel-inner").append(ImageDiv)
       }
 })
-<<<<<<< HEAD
-};
-=======
 }
 
 displayPhotos();
->>>>>>> a7c548af6d7f716c521d64db3b5b02a5f704813d

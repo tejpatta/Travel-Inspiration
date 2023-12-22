3eb6cbc24d4e0b44b1f80b47912a34f1a3a1126f
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-22 20:15:50 +0000

    change API key

--- assets/js/Unsplash.js
+++ assets/js/Unsplash.js
@@ -3,7 +3,7 @@ function displayPhotos(countryInput){
     let unsplashKey1 = "nkO4QtA3j8b37iVDzhJIitt4QwUCEv6UfCOB7-O2d1I";
     let unsplashKey2 = "__q3dGBVbr-ZPGFeEpdoAGNhbQ24nZxvv6gIazuJnsA";
     
-    let photoURL = "https://api.unsplash.com/search/photos/?client_id=" + unsplashKey1+ "&query=" + countryInput;
+    let photoURL = "https://api.unsplash.com/search/photos/?client_id=" + unsplashKey2+ "&query=" + countryInput;
     
     fetch(photoURL)
     .then(function (response) {

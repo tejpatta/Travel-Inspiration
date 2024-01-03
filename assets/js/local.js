d13252959f9a23de04a3440a4b939e81f4bcf644
Tejpatta <tejurawal@gmail.com>
2024-01-03 22:09:25 +0000

    added new logo to mylist

--- assets/js/local.js
+++ assets/js/local.js
@@ -10,14 +10,14 @@
 
     // Card
     for (i = 0; i < savedFavourites.length; i++) {
-    let favDiv = $("<div>").addClass("card mb-3")
+    let favDiv = $("<div>").addClass("card m-3 col-lg-4 col-md-4 col-sm-12")
     let favRow = $("<div>").addClass("row g-0")
     let favColImg = $("<div>").addClass("col-sm-4")
     let favImage = $("<img>").addClass("img-fluid rounded-start").attr("src", savedFavourites[i][1])
     let favColTxt = $("<div>").addClass("col-sm-8")
     let favBody = $("<div>").addClass("card-body")
-    let favTitle = $("<h5>").addClass("card-title").text(savedFavourites[i][0])
-    let favBtn = $("<a>").addClass("btn btn-primary").attr('id','view-saved')
+    let favTitle = $("<h5>").addClass("card-title").text((savedFavourites[i][0]).toUpperCase())
+    let favBtn = $("<a>").addClass("btn btn-primary").attr('id','view-saved').text("View")
     
     favColImg.append(favImage)
     favBody.append(favTitle, favBtn)
